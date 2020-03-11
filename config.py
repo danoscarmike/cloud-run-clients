6f247b29f7a4a7b4c6d00aa12de89b364340deb8
Dan O'Meara <omeara.dan@gmail.com>
2020-03-10 21:21:14 -0700

    prep for database

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 import tempfile
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     PATH_TO_API_COMMON_PROTOS = '/protos/'
@@ -9,5 +11,8 @@ class Config(object):
     DOWNLOAD_DIR = os.path.join(os.getcwd(),"download")
     CLIENT_DIR = os.path.join(os.getcwd(),"client")
     PROTO_DIR = os.path.join(os.getcwd(),"proto")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 
